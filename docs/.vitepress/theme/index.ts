6e7ee8d4c3fce7a30698c9468514746d4dd8f1ac
richie-rich90454 <rjiang880@gmail.com>
2026-08-12 08:48:25 +0800

    [DOCS] Import the KaTeX stylesheet through the default theme

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,7 @@
+import DefaultTheme from "vitepress/theme";
+import type { Theme } from "vitepress";
+import "katex/dist/katex.min.css";
+
+export default {
+    extends: DefaultTheme,
+} satisfies Theme;
